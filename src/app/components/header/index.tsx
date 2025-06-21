cc52ff0bea0bd9b1a0bfcb262b847727f05ab361
Camelia Osan <osan.camelia2001@gmail.com>
2025-06-21 15:48:09 +0300

    + conflicts resolved

--- src/app/components/header/index.tsx
+++ src/app/components/header/index.tsx
@@ -21,13 +21,14 @@ import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 
 
-const pages = ['Login', 'Register', 'Sellers', 'Checkout'];
+const pages = ['Login', 'Register', 'Sellers', 'Shop', 'Checkout'];
 const settings = ['Account', 'Dashboard', 'My Shop', 'Logout'];
 
 const pageRoutes: { [key: string]: string } = {
     'Login': '/login',
     'Register': '/register',
     'Sellers': '/sellersPage',
+    'Shop': '/shopPage',
     'Checkout': '/checkout'
 };
 
