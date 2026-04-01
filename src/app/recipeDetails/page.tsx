369a492921cffcd86983a4548fee49b88e974dc3
Camelia Osan <osan.camelia2001@gmail.com>
2026-04-02 02:21:35 +0300

    + recipe details css file

--- src/app/recipeDetails/page.tsx
+++ src/app/recipeDetails/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import styles from './page.module.css';
 import { FC } from "react";
 import Button from '@mui/material/Button';
 import GroupsIcon from '@mui/icons-material/Groups';
@@ -16,15 +17,7 @@ const RecipeDetails: FC = () => {
     return (
         <>
             {/* Navigation part - back button + add ingredients to cart */}
-            <div className="navigationContainer"
-                style={{
-                    display: 'flex',
-                    backgroundColor: 'white',
-                    justifyContent: 'space-between',
-                    alignItems: 'center',
-                    width: '100%',
-                    padding: '10px 50px'
-                }}>
+            <div className={styles.navigationContainer}>
                 <Button variant="outlined" startIcon={<ArrowBackIosIcon />}>
                     Back
                 </Button>
@@ -35,48 +28,16 @@ const RecipeDetails: FC = () => {
 
 
             {/* Recipe body */}
-            <div className="parentContainerRecipeDetails"
-                style={{
-                    backgroundColor: 'white',
-                    color: 'black',
-                    height: '100%',
-                    width: '100%',
-                    display: 'flex'
-                }}
-            >
+            <div className={styles.parentContainerRecipeDetails}>
                 {/* Recipe details - image + title + actions */}
-                <div className="detailsContainer"
-                    style={{
-                        height: '100%',
-                        width: '100%',
-                        padding: '10px 100px',
-                        display: 'flex',
-                        flexDirection: 'column',
-                        alignItems: 'flex-start',
-                        justifyContent: 'center'
-                    }}
-                >
-                    <div className="imageContainer" // To center the image
-                        style={{
-                            display: 'flex',
-                            justifyContent: 'center',
-                            alignItems: 'center',
-                            width: '100%',
-                            marginBottom: '20px'
-                        }}>
+                <div className={styles.detailsContainer}>
+                    <div className={styles.imageContainer}>
                         <img src="/images/bgplaceholder.jpeg" alt="Recipe Image" width={1025} height={500} />
                     </div>
 
-                    <div className="descriptionContainer"   // For the title and the actions
-                        style={{
-                            display: 'flex',
-                            justifyContent: 'space-between',
-                            alignItems: 'center',
-                            width: '100%',
-                            marginBottom: '15px'
-                        }}>
+                    <div className={styles.descriptionContainer}>
                         <h2 style={{ marginRight: '30px' }}>Chicken Ramen Noodles</h2>
-                        <span style={{ display: 'flex', alignItems: 'center', gap: '5px' }}>
+                        <span className={styles.actionsSpan}>
                             <Button variant="text" startIcon={<FavoriteBorderIcon />}>
                                 Save
                             </Button>
@@ -89,23 +50,13 @@ const RecipeDetails: FC = () => {
             </div>
 
             {/* Ingredients and instructions */}
-            <div className="parentContainerRecipeDetails"
-                style={{
-                    backgroundColor: 'white',
-                    color: 'black',
-                    height: '100%',
-                    width: '100%',
-                    display: 'flex',
-                    flexDirection: 'column',
-                    padding: '0 100px'
-                }}
-            >
-                <div style={{ display: 'flex', gap: '20px', marginBottom: '30px', alignItems: 'center' }}>
-                    <span style={{ display: 'flex', alignItems: 'center', gap: '5px' }}>
-                        <GroupsIcon style={{ margin: '0 8px' }} />3-4 servings
+            <div className={styles.parentContainerRecipeDetails}>
+                <div className={styles.actionsSpanContainer}>
+                    <span className={styles.actionsSpan}>
+                        <GroupsIcon style={{ margin: '0 8px' }} /> 3-4 servings
                     </span>
-                    <span style={{ display: 'flex', alignItems: 'center', gap: '5px' }}>
-                        <AccessTimeIcon style={{ margin: '0 8px' }} />30 minutes
+                    <span className={styles.actionsSpan}>
+                        <AccessTimeIcon style={{ margin: '0 8px' }} /> 30 minutes
                     </span>
                 </div>
 
