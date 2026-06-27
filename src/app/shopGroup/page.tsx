83d231136e6e227998a1b955ec2e9f2ac3208300
Camelia Osan <osan.camelia2001@gmail.com>
2026-06-28 02:11:18 +0300

    + shop group mvp

--- src/app/shopGroup/page.tsx
+++ src/app/shopGroup/page.tsx
@@ -6,11 +6,8 @@ import styles from './page.module.css';
 
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import CardActionArea from '@mui/material/CardActionArea';
-import CardActions from '@mui/material/CardActions';
+
 
 import SellerCard from "../components/sellerCard";
 
